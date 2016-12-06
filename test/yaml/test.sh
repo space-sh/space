0c8268ae8ba857e26e505ed0d39d4beca4cb7f53
Maicon Diniz Filippsen <filippsen@blockie.org>
2016-12-06 13:07:03 +0100

    Change YAML test to set and return correct exit status code on failure
    * Value returned on failure is equal to the number of errors that
    occurred. This is capped to a maximum of 255.
    
    - Remove usage of || construct for comparison in favor of if statement

--- test/yaml/test.sh
+++ test/yaml/test.sh
@@ -16,10 +16,39 @@
 # along with Space.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+#=======================
+# _CHECK_EXIT_STATUS
+#
+# Check for status code and increment a counter on error
+#
+# Parameters:
+#   $1: the status code to check
+#
+# Expects:
+#   _exit_status
+#
+#=======================
+_CHECK_EXIT_STATUS()
+{
+    local _status_code="$1"
+
+    if [ "$_status_code" -gt 0 ]; then
+        PRINT "  Failed!" "error"
+
+        # Update exit counter. Cap to 255 to prevent the value wrapping back
+        if [ "$_exit_status" -lt 255 ]; then
+            _exit_status=$((_exit_status + 1 ))
+        fi
+    else
+        PRINT "  [OK!]" "success"
+    fi
+
+}
+
 _PARSE_YAML()
 {
     # External
-    SPACE_CMDDEP="PRINT"
+    SPACE_CMDDEP="PRINT _CHECK_EXIT_STATUS"
 
     DIFF_BIN=diff
 
@@ -29,32 +58,35 @@ _PARSE_YAML()
     fi
 
     # Loop through all subdirs for testing
+    local _exit_status=0
     for _dir_name in ./test/yaml/* ; do
         if [ -d "$_dir_name" ]; then
             PRINT "Performing test: \"$_dir_name\" *"
 
             PRINT "  X1: preprocess step"
             ./space -C0 -f ./"$_dir_name"/input.yaml -X1 | \
-                $DIFF_BIN - ./"$_dir_name"/output_X1.yaml || (PRINT "  Failed!" "error"; exit 1)
-            PRINT "  [OK!]" "success"
+                $DIFF_BIN - ./"$_dir_name"/output_X1.yaml 
+            _CHECK_EXIT_STATUS "$?"
 
             PRINT "  X2: parse step"
             ./space -C0 -f ./"$_dir_name"/input.yaml -X2 | \
-                $DIFF_BIN - ./"$_dir_name"/output_X2.yaml || (PRINT "  Failed!" "error"; exit 1)
-            PRINT "  [OK!]" "success"
+                $DIFF_BIN - ./"$_dir_name"/output_X2.yaml
+            _CHECK_EXIT_STATUS "$?"
 
             PRINT "  X3: transform step"
             ./space -C0 -f ./"$_dir_name"/input.yaml -X3 | \
-                $DIFF_BIN - ./"$_dir_name"/output_X3.yaml || (PRINT "  Failed!" "error"; exit 1)
-            PRINT "  [OK!]" "success"
+                $DIFF_BIN - ./"$_dir_name"/output_X3.yaml
+            _CHECK_EXIT_STATUS "$?"
 
             PRINT "  X4: env eval step"
             ./space -C0 -f ./"$_dir_name"/input.yaml -X4 | \
-                $DIFF_BIN - ./"$_dir_name"/output_X4.yaml || (PRINT "  Failed!" "error"; exit 1)
-            PRINT "  [OK!]" "success"
+                $DIFF_BIN - ./"$_dir_name"/output_X4.yaml
+            _CHECK_EXIT_STATUS "$?"
 
             PRINT ""
         fi
     done
+
+    return "$_exit_status"
 }
 

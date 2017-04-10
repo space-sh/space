c91badf57bab05b677859098e302865b9580b4d9
Thomas Backlund <blund@blockie.org>
2017-04-10 10:30:54 +0200

    Update auto completion functionality

--- completion/init_autocompletion.sh
+++ completion/init_autocompletion.sh
@@ -26,9 +26,65 @@
 #   Spaces inside values isn't handled properly as for now.
 #   Quoted values on -e flags makes the rest of the line non completable.
 #   Can't handle -e -e var= because of the array concat.
+#   compopt -o nospace is not available in bash3, workaround?
 #
 #==========
 [ -n "${BASH_VERSION}" ] &&
+_space_dyn_comp()
+{
+    local timeout=$1
+    shift
+    local prefix=$1
+    shift
+
+    local shasbin=
+    local tag=
+    local status=
+    local ts=$(date +"%s")
+    if command -v sha256sum >/dev/null; then
+        shasbin=sha256sum
+    elif command -v sha1sum >/dev/null; then
+        shasbin=sha1sum
+    elif command -v shasum >/dev/null; then
+        shasbin="shasum -a 256"
+    else
+        shasbin=
+    fi
+    if [ -n "${shasbin}" ] && [ "${timeout}" -gt 0 ]; then
+        tag=$(echo "${prefix}${args_tag}" | ${shasbin} 2>/dev/null)
+        tag="${tag%% *}"
+        local filename="/tmp/space.$$.${tag}"
+        if [ -f "${filename}" ]; then
+            # Read file and decide if it's fresh or not
+            result="$(cat ${filename})"
+            local ts2=
+            ts2="${result%%$'\n'*}"
+            result="${result#*$'\n'}"
+            status="${result%%$'\n'*}"
+            if [ "${status}" = "${result}" ]; then
+                # To handle ignored newlines on empty result.
+                result=""
+            else
+                result="${result#*$'\n'}"
+            fi
+            if [ -n "${ts2}" ] && (( ts-ts2 < timeout )); then
+                return $status
+            fi
+            # Fall through
+        fi
+    fi
+
+    result="$($args)"
+    local status=$?
+    if [ "${status}" -ne 1 ] && [ -n "${tag}" ] && [ "${timeout}" -gt 0 ]; then
+        # Cache result.
+        printf "%s\n%s\n%s\n" "${ts}" "${status}" "${result}" > "${filename}"
+        if [ "$?" -gt 0 ]; then
+            rm -f "${filename}"
+        fi
+    fi
+    return $status
+} &&
 _space_join_arr()
 {
     local _dest="${1}"
@@ -39,25 +95,28 @@ _space_join_arr()
     local length="${1-}"
     shift || :
     if [ -z "${length}" ]; then
-        length="$(( ${#COMP_WORDS[@]} - $first ))"
+        length="$(( ${#COMP_WORDS[@]} - first ))"
     fi
     local A=("${COMP_WORDS[@]:$first:$length}")
     local sep=""
-    for line in "${A[@]}"
-    do
-        #if [ "${line}" = '=' ] || ([ "${last}" = '=' ] && [ "${line:0:1}" != "-" ] ); then
-        if [ "${line}" = '=' ] || [ "${last}" = '=' ]; then
-            sep=""
-        else
-            sep=" "
-        fi
-        eval "$_dest=\"\${$_dest}$sep\$line\""
-        last="${line}"
-    done
+    if [ "${#A[@]}" -gt 0 ]; then
+        for line in "${A[@]}"; do
+            if [ "${line}" = '=' ] || [ "${last}" = '=' ]; then
+                sep=""
+            else
+                sep=" "
+            fi
+            eval "$_dest=\"\${$_dest}$sep\$line\""
+            last="${line}"
+        done
+    fi
 } &&
 _space()
 {
-    local _switches="-f -m -a -M -e -d -p -v -h -V -C -U -X -k -K -S --"
+    local switches="-f -m -a -M -e -d -p -v -h -V -C -U -X -k -K -S --"
+    local timeout1="${SPACE_COMP_TIMEOUT1:-15}"
+    local timeout2="${SPACE_COMP_TIMEOUT2:-15}"
+    local args_tag=""
 
     # We might need to adjust the array to the Bash 4 format
     # where it splits on equal signs according to COMP_WORDBREAKS.
@@ -85,101 +144,105 @@ _space()
                         ((COMP_CWORD+=1))
                     fi
                 fi
-                COMP_WORDS+=("${A[@]}")
+                if [ "${#A[@]}" -gt 0 ]; then
+                    COMP_WORDS+=("${A[@]}")
+                fi
                 continue 2
             fi
         done
         break
     done
-    local _current=${COMP_WORDS[COMP_CWORD]}
-    local _previous=${COMP_WORDS[((COMP_CWORD-1))]}
+    local current=${COMP_WORDS[COMP_CWORD]}
+    local previous=${COMP_WORDS[((COMP_CWORD-1))]}
 
-    local _args=
+    local args=
     while true; do
         # Check if cursor is beyond a double dash (--).
-        local _counter=0
-        while (( _counter < COMP_CWORD )); do
-            if [ "${COMP_WORDS[$_counter]}" = "--" ]; then
+        local counter=0
+        while (( counter < COMP_CWORD )); do
+            if [ "${COMP_WORDS[$counter]}" = "--" ]; then
                 # TODO: we might want to escape this.
                 local part1=""
-                _space_join_arr "part1" "1" $(($COMP_CWORD-1))
-                local index=$((COMP_CWORD-_counter-1))
-                _args="${COMP_WORDS[0]} -v0 -6 $index=${_current} $part1"
+                _space_join_arr "part1" "1" $((COMP_CWORD-1))
+                local index=$((COMP_CWORD-counter-1))
+                args="${COMP_WORDS[0]} -v0 -6 $index=${current} $part1"
+                _space_join_arr "args_tag" "1" "$((COMP_CWORD-1))"
                 break 2
             fi
-            ((_counter+=1))
+            ((counter+=1))
         done
+        unset counter
 
-        if [[ ${_current} == "-" ]]; then
-            COMPREPLY=($(compgen -W "$_switches" -- "$_current"))
+        if [[ ${current} == "-" ]]; then
+            COMPREPLY=($(compgen -W "$switches" -- "$current"))
             return 0
         fi
 
-        if [[ ((${#_current} == 2)) && ${_current:0:1} == "-" && $_switches =~ "${_current}" ]]; then
+        if [[ ((${#current} == 2)) && ${current:0:1} == "-" && $switches =~ "${current}" ]]; then
             # Complete with space, to keep stepping.
-            COMPREPLY=("$_current ")
+            COMPREPLY=("$current")
             return 0
         fi
         for s in "-K 0 1 2" "-X 1 2 3 4" "-v 0 1 2 3 4" "-C 0 1 2" "-k 0 1 2" ""; do
             local flag="${s%% *}"
             local options="${s#* }"
             for option in ${options}; do
-                if ([ "${_previous}" == "${flag}" ] && [ "${_current}" == "${option}" ]) || [ "${_current}" == "${flag}${option}" ]; then
-                    COMPREPLY=("$_current ")
+                if ([ "${previous}" == "${flag}" ] && [ "${current}" == "${option}" ]) || [ "${current}" == "${flag}${option}" ]; then
+                    COMPREPLY=("$current")
                     return 0
                 fi
             done
-            if [ "${_previous}" == "${flag}" ]; then
-                COMPREPLY=($(compgen -W "${options}" -- $_current))
+            if [ "${previous}" == "${flag}" ]; then
+                COMPREPLY=($(compgen -W "${options}" -- $current))
                 return 0
             fi
         done
 
-        if [ "${_current}" == "-f" ]; then
-            COMPREPLY=("$_current ")
-            return 0
-        fi
+        #if [ "${current}" == "-f" ]; then
+            #COMPREPLY=("$current")
+            #return 0
+        #fi
 
         # We want to separate switch from argument,
         # so that further completion works in a normalized manner.
-        if [[ ((${#_current} > 2)) && ${_current:0:1} == "-" && $_switches =~ "${_current:0:2}" ]]; then
-            COMPREPLY=("${_current:0:2} ${_current:2}")
+        if [[ ((${#current} > 2)) && ${current:0:1} == "-" && $switches =~ "${current:0:2}" ]]; then
+            command -v compopt >/dev/null && compopt -o nospace
+            COMPREPLY=("${current:0:2} ${current:2}")
             return 0
         fi
 
-        if [[ ${_previous} == "-f" ]]; then
-            if [ -f "${_current}" ]; then
-                COMPREPLY=("$_current ")
+        if [[ ${previous} == "-f" ]]; then
+            if [ -f "${current}" ]; then
+                COMPREPLY=("$current ")
             else
-                COMPREPLY=($(compgen -G "$_current*"))
+                COMPREPLY=($(compgen -G "$current*"))
             fi
             return 0
         fi
 
-        if [[ ${_previous} == "-m" || ${_previous} == "-M" ]]; then
-            if [[ ${_current} == "" ]]; then
-                _current="."
-            fi
-            _args="${COMP_WORDS[0]} -v0 -3 ${_current}"
+        if [[ ${previous} == "-m" || ${previous} == "-M" ]]; then
+            args="${COMP_WORDS[0]} -v0 -3 ${current:-.}"
+            timeout1=0
             break
         fi
-        if [ "${_previous}" = "-e" ]; then
-            if [[ ${_current} == "" ]]; then
-                _current="."
-            fi
+        if [ "${previous}" = "-e" ]; then
             local part1=""
             _space_join_arr "part1" "1"
-            _args="${COMP_WORDS[0]} -v0 -4 ${_current} ${part1}"
+            args="${COMP_WORDS[0]} -v0 -4 . ${part1}"
+            timeout1=0
+            command -v compopt >/dev/null && compopt -o nospace
             break
         fi
-        if [ "${_current}" = "=" ]; then
+        if [ "${current}" = "=" ]; then
+            current=""
             if [ "${COMP_CWORD}" -ge 1 ]; then
                 local _aprev="${COMP_WORDS[((COMP_CWORD-1))]}"
                 if [ "${#_aprev}" -gt 2 ] && [ "${_aprev:0:2}" = "-e" ]; then
                     # Complete on empty value.
                     local part1=""
                     _space_join_arr "part1" "1"
-                    _args="${COMP_WORDS[0]} -v0 -5 ${_aprev:2}= ${part1}"
+                    args="${COMP_WORDS[0]} -v0 -5 ${_aprev:2}= ${part1}"
+                    _space_join_arr "args_tag" "1" "$((COMP_CWORD))"
                     break
                 fi
             fi
@@ -188,19 +251,21 @@ _space()
                     # Complete on empty value.
                     local part1=""
                     _space_join_arr "part1" "1"
-                    _args="${COMP_WORDS[0]} -v0 -5 ${_previous}= ${part1}"
+                    args="${COMP_WORDS[0]} -v0 -5 ${previous}= ${part1}"
+                    _space_join_arr "args_tag" "1" "$((COMP_CWORD))"
                     break
                 fi
             fi
         fi
-        if [ "${_previous}" = "=" ]; then
+        if [ "${previous}" = "=" ]; then
             if [ "${COMP_CWORD}" -ge 2 ]; then
                 local _aprev="${COMP_WORDS[((COMP_CWORD-2))]}"
                 if [ "${#_aprev}" -gt 2 ] && [ "${_aprev:0:2}" = "-e" ]; then
                     # Complete on value.
                     local part1=""
                     _space_join_arr "part1" "1"
-                    _args="${COMP_WORDS[0]} -v0 -5 ${_aprev:2}=${_current} ${part1}"
+                    args="${COMP_WORDS[0]} -v0 -5 ${_aprev:2}=${current} ${part1}"
+                    _space_join_arr "args_tag" "1" "$((COMP_CWORD-1))"
                     break
                 fi
             fi
@@ -210,7 +275,8 @@ _space()
                     local _prevprev="${COMP_WORDS[((COMP_CWORD-2))]}"
                     local part1=""
                     _space_join_arr "part1" "1"
-                    _args="${COMP_WORDS[0]} -v0 -5 ${_prevprev}=${_current} ${part1}"
+                    args="${COMP_WORDS[0]} -v0 -5 ${_prevprev}=${current} ${part1}"
+                    _space_join_arr "args_tag" "1" "$((COMP_CWORD-1))"
                     break
                 fi
             fi
@@ -218,53 +284,58 @@ _space()
 
         # Assume node completion.
         # Set empty "node" to root node.
-        if [[ ${_current} == "" ]]; then
-            _current="/"
+        if [[ ${current} == "" ]]; then
+            current="/"
         fi
         local part1=""
-        _space_join_arr "part1" "1" "$(($COMP_CWORD-1))"
+        _space_join_arr "part1" "1" "$((COMP_CWORD-1))"
         local part2=""
-        _space_join_arr "part2" "$(($COMP_CWORD+1))"
-        _args="${COMP_WORDS[0]} -v0 ${part1} ${_current} -1 ${part2}"
+        _space_join_arr "part2" "$((COMP_CWORD+1))"
+        args="${COMP_WORDS[0]} -v0 ${part1} ${current} -1 ${part2}"
+        timeout1=0
+        command -v compopt >/dev/null && compopt -o nospace
         break
-
-        return 1
     done
 
     #########################################
     # Do dynamic completion by calling space.
+    #
 
     # Run space completion in subshell.
     local result=
-    result="$($_args)"
+    _space_dyn_comp $timeout1 "first"
     local status=$?
-    if (( $status == 2 )); then
+    if (( status == 2 )); then
         # Dynamic completion.
+        if [[ $result =~ ^[123]:G$ ]]; then
+            COMPREPLY=($(compgen -G "$current*"))
+            return 0
+        fi
         local part1=
         _space_join_arr "part1" "1"
-        _args="${COMP_WORDS[0]} -v0 $result ${part1}"
+        args="${COMP_WORDS[0]} -v0 $result ${part1}"
         local result=
-        result="$($_args)"
+        local result=
+        _space_dyn_comp $timeout2 "second"
         local status=$?
-        if [ "${_current}" = "=" ]; then
-            _current=""
-        fi
         local a=()
         while IFS=$'\n' read -r _line; do
-            if [[ -n $_line ]] && [[ $_line =~ ^${_current}.* ]]; then
+            if [[ -n $_line ]] && [[ $_line =~ ^${current}.* ]]; then
                 a+=("${_line}")
             fi
         done <<< "${result}"
-        if [ "${#a[@]}" -eq 1 ]; then
-            _current="${a[0]}"
-            COMPREPLY=("$_current ")
+        if [ "${#a[@]}" -gt 0 ]; then
+            local _options=
+            printf -v _options "%s " "${a[@]}"
+            COMPREPLY=( $(compgen -W "${_options}" -- "${current}") )
             return $status
         fi
-        local _options=
-        printf -v _options "%s " "${a[@]}"
-        COMPREPLY=( $(compgen -W "${_options}" -- "${_current}") )
-        return $status
-    elif (( $status > 0 )); then
+        return 1
+    elif (( status == 3 )); then
+        # This is a special case of rewriting the current word.
+        # Fall through.
+        :
+    elif (( status > 0 )); then
         return 1
     fi
 
@@ -274,28 +345,27 @@ _space()
     done <<< "${result}"
 
     if (( ${#_items[@]} > 0 )); then
-        local _default=${_items[0]}
-        if [[ $_default == "." ]]; then
-            _default=""
+        if [ "${#_items[@]}" -eq 1 ] && [ -z "${_items[0]}" ]; then
+            return 1
         fi
         if [ "${#_items[@]}" -eq 1 ]; then
-            if [ -z "${_items[0]}" ]; then
-                return 1
-            fi
-            COMPREPLY=("${_items[0]} ")
-            return 0
+            #command -v compopt >/dev/null && compopt +o nospace
+            current="${_items[0]}"
+            local _options="${_items[@]}"
+            #COMPREPLY=("${current} ")
+            #return 0
+        elif (( status == 3 )); then
+            current="${_items[0]}"
+            local _options="${_items[@]:1}"
+        else
+            local _options="${_items[@]}"
         fi
-        if [ "${#_items[@]}" -eq 2 ]; then
-            COMPREPLY=("${_items[1]}")
-            return 0
-        fi
-        local _options="${_items[@]:1}"
-        COMPREPLY=( $(compgen -W "${_options}" -- "${_default}") )
+
+        COMPREPLY=( $(compgen -W "${_options}" -- "${current}") )
         return 0
     fi
-
     return 1
-} && complete -o nospace -F _space space
+} && complete -F _space space
 :
 
 # ex: filetype=sh

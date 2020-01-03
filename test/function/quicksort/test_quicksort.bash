ebc9b8862b784e0b0cc6d02883c0140ff266c7e8
Maicon Diniz Filippsen <filippsen@blockie.org>
2020-01-04 00:46:12 +0100

    Surround quicksort test call with locale settings to replicate actual use case

--- test/function/quicksort/test_quicksort.bash
+++ test/function/quicksort/test_quicksort.bash
@@ -27,7 +27,25 @@ _TEST_QUICKSORT()
     local _sorted_array=("2" "654" "200000" "_ae" "_yo_namespace" "c" "d" "dsijewr" "e" "mc_R4men" "no_marrianne" "paleo_chicken" "sq3" "y" "z" "zxczxcwqewqee")
     local _quicksort_output=()
 
+    # Store current locale settings
+    local _unset_locale_flag=0
+    local _stored_locale=
+    if [ -z "${LC_ALL+unset}" ]; then
+        _unset_locale_flag=1
+    else
+        _stored_locale="${LC_ALL}"
+    fi
+    # Set locale to get traditional sort order (bytes comparison)
+    export LC_ALL="C"
+
     _quicksort "${_unsorted_array[@]}"
+
+    # Restore locale settings to their original state
+    if [ "${_unset_locale_flag}" -eq "1" ]; then
+        unset LC_ALL
+    else
+        LC_ALL="${_stored_locale}"
+    fi
 
     if [[ "${_quicksort_output[*]}" == "${_sorted_array[*]}" ]]; then
         PRINT "_quicksort OK!" "ok"
